ccfbaa7683fd310babe9a0d6885f61a5b60eee6d
Oscar Ignacio Osses Arriaza <oscarossesa@gmail.com>
2015-05-14 00:47:58 -0400

    .

---
+++ Dominio/DominioPersonal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using Persistencia;
+
+namespace Dominio
+{
+    public class DominioPersonal
+    {
+        public bool AgregarPersonal(Personal personal, string path)
+        {
+            //TODO: implementar logica de dominio para agregar a una persona de tipo Personal
+
+            if (String.IsNullOrEmpty(personal.Nombre))
+            {
+                throw new ArgumentException("Nombre es nulo o vacío.");
+            }
+
+            if (!(Convert.ToInt32(personal.Anexo) > 0))
+            {
+                throw new ArgumentException("Anexo debe ser mayor que 0.");
+            }
+
+            //TODO: persistir el objeto Personal 
+            PersistirPersonal pPersonal = new PersistirPersonal();
+            pPersonal.AgregarPersonal(personal, path);
+
+
+            return true;
+        }
+    }
+}
---
+++ Persistencia/PersistirPersonal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Persistencia
+{
+    public class PersistirPersonal
+    {
+        public bool AgregarPersonal(Personal personal, string path)
+        {
+            //TODO: persistir obejeto de tipo Personal
+
+            LoadJson2(path);
+
+            return true;
+        }
+
+        public void LoadJson1()
+        {
+
+            //var path = Server.MapPath("~/Content/treatments.json");
+            using (StreamReader r = new StreamReader("path"))
+            {
+                string json = r.ReadToEnd();
+                List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+            }
+        }
+
+        public void LoadJson2(string path)
+        {
+            string json = System.IO.File.ReadAllText(path);
+            dynamic jsonObj = JsonConvert.DeserializeObject(json);
+            jsonObj["Personal"][0]["Nombre"] = "Oscar Osses Arriaza";
+            string output = JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+            System.IO.File.WriteAllText(path, output);
+        }
+    }
+
+    public class Item
+    {
+        public int millis;
+        public string stamp;
+        public DateTime datetime;
+        public string light;
+        public float temp;
+        public float vcc;
+    }
+}
